06abba595f9808d704a84b65f8bbdfb6bee6e6db
alvaromarquez-iesnervion <alvaro.marquez@iesnervion.es>
2025-10-09 13:56:57 +0200

    Ejercicio 3 completado

--- Tema4/03views/app/index.tsx
+++ Tema4/03views/app/index.tsx
@@ -1,70 +1,66 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  
-  
-  
   return (
-    <SafeAreaView>
-
+    <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.texto}>Header</Text>
       </View>
-      
+
       <View style={styles.content}>
-      <View style={styles.left}></View>
-      <View style={styles.center}>
-        <Text style={styles.texto}>Content</Text>
-      </View>
-      <View style={styles.right}></View>
-        
+        <View style={styles.left} />
+        <View style={styles.center}>
+          <Text style={styles.texto}>Content</Text>
+        </View>
+        <View style={styles.right} />
       </View>
 
-      <View style= {styles.footer}>
-        <Text style={styles.texto}>footer</Text>
+      <View style={styles.footer}>
+        <Text style={styles.texto}>Footer</Text>
       </View>
-      
-    </SafeAreaView> 
-
+    </SafeAreaView>
   );
-
-
 }
 
-const styles=StyleSheet.create({
-    container: {
-    flex: 1,                    
-    backgroundColor: "#fff",
-    },
-
-    header:{
-      flex: 80,
-      backgroundColor: "#ff0000ff"
-    },
-    content:{
-      flex: 1,
-      backgroundColor: "#0066ffff"
-    },
-    left:{
-      
-
-    },
-    right:{
-      
-
-    },
-
-    center:{
-      flex:1,
-
-    },
-    footer:{
-      flex: 80,
-      backgroundColor: "#00ffb3ff"
-    },
-    texto:{
-      justifyContent: "center"
-    }
-
-  })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eee",
+  },
+  header: {
+    backgroundColor: "aqua",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "gray",
+  },
+  left: {
+    width: 40,
+    backgroundColor: "blue",
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  right: {
+    width: 40,
+    backgroundColor: "green",
+  },
+  footer: {
+    backgroundColor: "pink",
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  texto: {
+    color: "blue",
+    fontWeight: "bold",
+  },
+});
