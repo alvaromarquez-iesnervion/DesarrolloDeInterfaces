9edccf9ca870a284db17a286fc9ea27d684d5e99
alvaromarquez-iesnervion <alvaro.marquez@iesnervion.es>
2025-10-21 13:05:50 +0200

    espacios

--- Tema5/PeopleListDI/app/Core/container.ts
+++ Tema5/PeopleListDI/app/Core/container.ts
@@ -1,16 +1,13 @@
-
-
-
 import { Container } from "inversify";
 import "reflect-metadata";
 import { TYPES } from "./types";
-import { IRepositoryPersonas, PersonasRepository } from "../Models/Data/personaRepository";
+import { IRepositoryPersonas, PersonasRepository, PersonasRepository100 } from "../Models/Data/personaRepository";
 import { PeopleListVM } from "../ViewModels/PeopleListVM";
 
 const container = new Container();
 
 
 // Vinculamos la interfaz con su implementación concreta
-container.bind<IRepositoryPersonas>(TYPES.IRepositoryPersonas).to(PersonasRepository);
+container.bind<IRepositoryPersonas>(TYPES.IRepositoryPersonas).to(PersonasRepository100);
 container.bind<PeopleListVM>(TYPES.IndexVM).to(PeopleListVM);
 export { container };
--- Tema5/PeopleListDI/app/ViewModels/PeopleListVM.ts
+++ Tema5/PeopleListDI/app/ViewModels/PeopleListVM.ts
@@ -5,44 +5,25 @@ import { Persona } from "../Models/Entities/Persona";
 import { TYPES } from "../Core/types";
 
 
-
 export class PeopleListVM {
-
 
     private _personasList: Persona[] = [];
     private _personaSeleccionada: Persona;
    
-
-
     constructor(
         @inject(TYPES.IRepositoryPersonas)
         private RepositoryPersonas: IRepositoryPersonas
     ) {
-
-
-       
         this._personaSeleccionada = new Persona(0, '', '');
-
-
         this._personasList = this.RepositoryPersonas.getListadoCompletoPersonas();
-     
     }
-
-
     public get personasList(): Persona[] {
         return this._personasList;
     }
-
-
     public get personaSeleccionada(): Persona {
         return this._personaSeleccionada;
     }
-
-
     public set personaSeleccionada(value: Persona) {
-        this._personaSeleccionada = value;
-       
+        this._personaSeleccionada = value; 
     }
-
-
   }
